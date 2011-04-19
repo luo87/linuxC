f361d25b1d88b6b88f065c0b8df640c310d6be9b
noname@ubuntu <luobailiang@gmail.com>
2011-04-20 00:48:24 +0800

    test exam3-11

--- exam3-11.c
+++ exam3-11.c
@@ -3,9 +3,15 @@
 int main(){
 	char peoples[] = "ABCD";	
 	for(int i = 0; i < strlen(peoples);i++){
-		
+		char killer = peoples[i];
+		int sum = (killer != 'A') + (killer == 'C') + (killer == 'D') +(killer != 'D');
+		//printf("%d\n", sum);
+		if(sum == 3){
+			printf("%c is killer\n", killer);
+			break;
+		}	
 	}
-	string hello = "dfdf";
-	printf("sizeof peoples is : %d\n", strlen(peoples));
+//	string hello = "dfdf";
+//	printf("sizeof peoples is : %d\n", strlen(peoples));
 	return 0;
 }
