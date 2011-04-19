8e09d5f5869fc0be571737aff93a9add305de749
noname@ubuntu <luobailiang@gmail.com>
2011-04-20 00:16:55 +0800

    add

---
+++ exam3-11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char peoples[] = "ABCD";	
+	for(int i = 0; i < strlen(peoples);i++){
+		
+	}
+	string hello = "dfdf";
+	printf("sizeof peoples is : %d\n", strlen(peoples));
+	return 0;
+}
