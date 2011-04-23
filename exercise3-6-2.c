ebe21418cb436b88fa9c8161ae1c38c448b74792
noname@ubuntu <luobailiang@gmail.com>
2011-04-23 18:08:19 +0800

    add

---
+++ exercise3-6-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char peoples[] = "ABCDE";
+	int i;
+	for(i = 0; i < strlen(peoples); i++){
+		char people = peoples[i];
+		int level;
+		for(level = 1; level <= 5; level++){
+			//bool A = ();
+			printf("df");
+		}
+	}
+}
